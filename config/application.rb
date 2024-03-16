4f8982507dacfc0475f5c2b4a153f5a129e58c42
Juan Gonzalez <jgonzalez.wj@gmail.com>
2024-03-15 19:51:39 -0600

    Config routes for vets and clients, add autoload path for users

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module Univet
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
+    config.autoload_paths << Rails.root.join("/app/models/users")
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
