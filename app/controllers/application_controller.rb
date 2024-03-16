72819ab938de88caf384642777e1599beb845a6e
Juan Gonzalez <jgonzalez.wj@gmail.com>
2024-03-15 19:54:17 -0600

    Set fields for STI and update permitted params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::API
     before_action :configure_permitted_paremeters, if: :devise_controller?
+    devise_group :user, contains: [:vet, :client]
 
     def configure_permitted_paremeters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :name])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :type])
     end
 end
