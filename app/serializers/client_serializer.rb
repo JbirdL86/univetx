b48944f116329bdb7532338b291e921075ef6829
Juan Gonzalez <jgonzalez.wj@gmail.com>
2025-08-05 21:16:12 -0600

    Adjust client serializer not to show password

--- app/serializers/client_serializer.rb
+++ app/serializers/client_serializer.rb
@@ -1,6 +1,7 @@
 class ClientSerializer
   include JSONAPI::Serializer
-  attributes :id, :email, :password, :password_confirmation,:type, :name, :last_name, :address, :country, :city
+  attributes :id, :email, :type, :name, :last_name, :address, :country, :city
+  
   has_many :afiliations 
   has_many :vets, through: :afiliations
   has_many :animals
